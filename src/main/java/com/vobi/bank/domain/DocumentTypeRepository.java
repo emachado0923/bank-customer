37ff28d3b32ab6a3028ea62effd3a86b1c03e399
Emerson Machado <64100237+emachado0923@users.noreply.github.com>
2022-02-12 08:30:21 -0500

    First Commit

---
+++ src/main/java/com/vobi/bank/domain/DocumentTypeRepository.java
@@ -0,0 +1,7 @@
+package com.vobi.bank.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentTypeRepository extends JpaRepository<DocumentType, Integer> {
+
+}
